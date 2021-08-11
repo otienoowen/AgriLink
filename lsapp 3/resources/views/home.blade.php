2ade6f7b9465f469f81a1f6499443bd327d1e86e
Andesia <61774115+Andesia@users.noreply.github.com>
2021-08-11 10:21:32 +0300

    admin-crud

---
+++ lsapp 3/resources/views/home.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('You are logged in!') }}
+                            <hr>
+                    <a href="/posts/create" class="btn btn-primary">Create Posts</a>
+                    <h3>Your blog posts</h3>
+
+                    <p>{{Auth::user()->name}}</p>
+                    <p></p>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
