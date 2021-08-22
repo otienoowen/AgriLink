53c3e9c4800c24b10fe024525fc679537bc29c54
Andesia <61774115+Andesia@users.noreply.github.com>
2021-08-22 15:41:29 +0300

    Revert "delete lsapp 3"

---
+++ lsapp 3/resources/views/admin/create.blade.php
@@ -0,0 +1,105 @@
+@extends('admin.layout')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            @if(session('status'))
+                <h6 class="alert alert-success">{{session('status')}}</h6>
+            @endif
+            <div class="card">
+                <div class="card-header">
+                    <h4>Add user with IMAGE
+                        <a href="{{url('admin')}}" class="btn btn-danger float-right">BACK</a>
+                    </h4> 
+                </div> 
+                <div class="card-body">
+                    <form action="{{url ('/add-user')}}" method="POST" enctype="multipart/form-data">
+                        @csrf
+                       
+                         <div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                                @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                         
+                         <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="user_type" class="col-md-4 col-form-label text-md-right">User Type</label>
+
+                            <div class="col-md-6">
+                                <input id="user_type" type="string" class="form-control @error('user_type') is-invalid @enderror" name="user_type" value="" required autocomplete="user_type">
+
+                                @error('user_type')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for=""class="col-md-4 col-form-label text-md-right">Image</label>
+
+                            <div class="col-md-6">
+                            <input type="file" name="profile_image" class="form-control" required>
+                            </div>                            
+                         </div> 
+
+                         <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary"> Add User </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>             
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
