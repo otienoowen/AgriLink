fb0b76e78d88ba916ea5191fe87cdac4b4afd7de
Andesia <lihasigrace@gmail.com>
2021-08-31 02:21:41 +0300

    test 1 fixes

--- resources/views/Layouts/newapp.blade.php
+++ resources/views/Layouts/newapp.blade.php
@@ -12,7 +12,7 @@
 
 <!-- Scripts -->
 <script src="{{ asset('js/script.js') }}" defer></script>
-<script src="{{ asset('js/app.js') }}" defer></script>
+{{-- <script src="{{ asset('js/app.js') }}" defer></script> --}}
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" ></script>
     <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js">
     </script>
